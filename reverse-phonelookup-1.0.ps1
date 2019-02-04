ada046977e6de57741e33886439401967d8bae42
gpoguy234 <gpoguy234@gmail.com>
2019-02-03 23:18:36 -0700

    reverse phone lookup helper
    powershell tool that calls the yellowpages website to do a quick reverse lookup of Canadian numbers

---
+++ reverse-phonelookup-1.0.ps1
@@ -0,0 +1,12 @@
+﻿$phoneNum = Read-Host "enter phone # (Use dashes: ie. 1-234-567-8900) "
+
+
+#$URL = "http://canada411.yellowpages.ca/fs/X-XXX-XXX-XXXX?what=X-XXX-XXX-XXXX"
+
+$URLPart1 = "http://canada411.yellowpages.ca/fs/"
+$URLPart2 = $phoneNum
+$URLPart3 = "?what="
+$URLPart4 = $URLPart2
+$URL = $URLPart1 + $URLPart2 + $URLPart3 + $URLPart4
+
+Start-Process chrome.exe $URL
