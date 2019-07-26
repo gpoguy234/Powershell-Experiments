7a5bb0a89e91eb8fcb9a60b3e4259ba5eed72e0c
gpoguy234 <gpoguy234@gmail.com>
2019-07-26 15:44:37 -0600

    Add files via upload
    Initial commit - v.0.1 - search o365 message trace for emails from a particular class C subnet range

---
+++ Recursive-message-subnet-sweep.ps1
@@ -0,0 +1,9 @@
+﻿$prefix = Read-Host "enter class c subnet prefix "
+$x = (160..215)
+$StartD = (get-date).AddDays(-9).toString("MM/dd/yyy")
+$EndD = (get-date).AddDays(1).toString("MM/dd/yyy")
+
+foreach($item in $x) {
+	$ip = $prefix+$item;
+	Get-MessageTrace -FromIP $ip -StartDate $StartD -EndDate $EndD | select -Property Received, SenderAddress, RecipientAddress, subject, status | Export-Csv -NoTypeInformation -Append -path $prefix".x.csv"
+}
