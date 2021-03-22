0c6d823468187dbff362ab7b3d22c1797193be2b
gpoguy234 <gpoguy234@gmail.com>
2021-03-21 22:53:47 -0600

    get-events-from-eventlog.ps1 added for event log testing
    basic queries to expand on for log hunting

---
+++ get-events-from-eventlog.ps1
@@ -0,0 +1,4 @@
+﻿Get-WinEvent -LogName OpenSSH/Operational -MaxEvents 10000 | Where { $_.Message -match "Accepted|Failed|Starting" } | fl -Property TimeCreated,Message
+
+Get-WinEvent -LogName Application -MaxEvents 10000 | Where { $_.Message -match "fail" } | fl -Property TimeCreated,Message
+
