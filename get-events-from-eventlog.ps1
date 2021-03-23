cc42936d65c686a0d70293d1b1e960500499a1b1
gpoguy234 <gpoguy234@gmail.com>
2021-03-22 21:45:16 -0600

    added security event log queries by event ID

--- get-events-from-eventlog.ps1
+++ get-events-from-eventlog.ps1
@@ -2,3 +2,6 @@
 
 Get-WinEvent -LogName Application -MaxEvents 10000 | Where { $_.Message -match "fail" } | fl -Property TimeCreated,Message
 
+Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4624|4625|4627|4634|4648|4672" } | fl -Property TimeCreated,Message
+
+Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4625" } | fl -Property TimeCreated,Message
