dda5ef1b5509475c4d21d8dcfbc1f992a3a34a47
gpoguy234 <gpoguy234@gmail.com>
2021-04-09 11:54:01 -0600

    Update find-userStuff.ps1
    added search capability for all user profiles

--- find-userStuff.ps1
+++ find-userStuff.ps1
@@ -1,4 +1,7 @@
-# find stuff fast un user profiles
-dir -path C:\Users -Directory |
-    foreach { dir -Path $_.FullName -File -Recurse -Include *.tiff,*.btu,*.txt |
+# find stuff fast un user profiles  - must be running as local admin or System user
+$Profiles = dir C:\Users -Directory
+foreach ($UserFolder in $Profiles){
+    dir -path C:\Users\$UserFolder -Directory |
+        foreach { dir -Path $_.FullName -File -Recurse -Include *.tiff,*.btu,*.txt |
                 Select-Object -ExpandProperty FullName }
+}
