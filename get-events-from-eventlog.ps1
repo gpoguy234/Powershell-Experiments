8f0024f642579fe29661877ad77e20d6c1195dab
gpoguy234 <gpoguy234@gmail.com>
2021-05-06 08:44:28 -0600

    added reference for parsing events from file by ID

--- get-events-from-eventlog.ps1
+++ get-events-from-eventlog.ps1
@@ -5,3 +5,5 @@ Get-WinEvent -LogName Application -MaxEvents 10000 | Where { $_.Message -match "
 Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4624|4625|4627|4634|4648|4672" } | fl -Property TimeCreated,Message
 
 Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4625" } | fl -Property TimeCreated,Message
+
+Get-WinEvent -path <pathtofile>\security.evtx -MaxEvents 10000 | Where { $_.Id -match "4624|4625|4627|4634|4648|4672" } | fl -Property TimeCreated,Message
