b3808a85f4e43e787ec3e2106c79dca10f0f7848
gpoguy234 <gpoguy234@gmail.com>
2021-05-19 20:50:33 -0600

    Update get-events-from-eventlog.ps1
    more examples added

--- get-events-from-eventlog.ps1
+++ get-events-from-eventlog.ps1
@@ -7,3 +7,9 @@ Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4624|462
 Get-WinEvent -LogName Security -MaxEvents 10000 | Where { $_.Id -match "4625" } | fl -Property TimeCreated,Message
 
 Get-WinEvent -path <pathtofile>\security.evtx -MaxEvents 10000 | Where { $_.Id -match "4624|4625|4627|4634|4648|4672" } | fl -Property TimeCreated,Message
+
+#send to file
+Get-WinEvent -LogName Application -MaxEvents 10000 | Where { $_.Message -match "fail" } | export-csv -path results.csv
+
+#generic string for flexible search queries
+Get-WinEvent -path <evtx file>  -MaxEvents 10000 | Where { $_.Message -match "<keyword>" } | fl -Property TimeCreated,Message
