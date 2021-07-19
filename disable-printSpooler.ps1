14aa4a8cbae59dff3c026239ca85bee4ccfcb4d3
gpoguy234 <gpoguy234@gmail.com>
2021-07-19 16:57:57 -0600

    quick fix script CVE-2021-34481
    quick fix it up script to flip dat lip...

---
+++ disable-printSpooler.ps1
@@ -0,0 +1,12 @@
+﻿#Windows Print Spooler Elevation of Privilege Vulnerability
+#CVE-2021-34481
+
+if((Get-Service -Name Spooler).Status -eq 'Running'){
+    #If the Print Spooler is running or if the service is not disabled, follow these steps:
+    #Stop and disable the Print Spooler service
+    #If stopping and disabling the Print Spooler service is appropriate for your environment, run the following in Windows PowerShell:
+    Stop-Service -Name Spooler -Force
+
+    Set-Service -Name Spooler -StartupType Disabled
+}
+else{ write-host "already disabled - your safe!"}
