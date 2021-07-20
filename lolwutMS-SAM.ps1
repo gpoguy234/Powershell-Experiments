2a9001a86dd0d3623caeb61fc5a23b167788f650
gpoguy234 <gpoguy234@gmail.com>
2021-07-19 22:39:58 -0600

    test SAM permissions
    so many windows problems this month - gotta catch em all!

---
+++ lolwutMS-SAM.ps1
@@ -0,0 +1,24 @@
+﻿# Description: Check for new SAM vuln - fully patched windows 10/11 systems seeme to be vulnerable to allowing any user to access the SAM database
+# Date: 07/19/2021
+
+<#
+C:\Users\User>icacls c:\Windows\System32\config\SAM
+c:\Windows\System32\config\SAM BUILTIN\Administrators:(I)(F)
+                               NT AUTHORITY\SYSTEM:(I)(F)
+                               BUILTIN\Users:(I)(RX)
+                               APPLICATION PACKAGE AUTHORITY\ALL APPLICATION PACKAGES:(I)(RX)
+                               APPLICATION PACKAGE AUTHORITY\ALL RESTRICTED APPLICATION PACKAGES:(I)(RX)
+
+Successfully processed 1 files; Failed processing 0 files
+#>
+$searchStr = [regex]::Escape("Users:(I)(RX)")
+
+$check = icacls c:\Windows\System32\config\SAM | Out-String | Select-String -Pattern "Users" -context 1
+#get-acl -path c:\Windows\System32\config\SAM -Audit
+
+if( $check | select-string $searchStr ) {
+    write-host "Looks like Users have some privs `n $check "
+    }
+else{
+    write-host "your probably safe"
+    }
