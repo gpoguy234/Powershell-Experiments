353373c6662051b82d3d7dbe004ee196a27efed9
gpoguy234 <gpoguy234@gmail.com>
2021-08-11 10:33:35 -0600

    identify domain trusts
    script to identify one-way domain trusts

---
+++ get-domain-trust-status.ps1
@@ -0,0 +1,17 @@
+﻿#$uDomain = "testing.local"  #static domain name
+
+#prompt for trusted domain 
+$uDomain = Read-Host "Enter Remote Domain Name: (i.e. scada.local)"
+#get Domain trust information
+$dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+try{$status = $dom.GetSelectiveAuthenticationStatus($uDomain)}
+catch {$status = $False}
+if ($status -eq $True){
+    Write-Host "Domain trust is in place between $dom and $uDomain"
+    $dom.GetAllTrustRelationships()
+}
+else {
+    Write-host "A domain trust relationship does not exist between $dom and $uDomain" -ForegroundColor DarkGreen
+    
+}
