a554de93000f20663a9a93e701b156d53e7c6563
gpoguy234 <gpoguy234@gmail.com>
2021-11-23 17:25:19 -0700

    Update get-eventlogs.ps1
    added additional log collection and check for c:\temp folder

--- get-eventlogs.ps1
+++ get-eventlogs.ps1
@@ -1,9 +1,9 @@
-﻿############################################################################################################
+############################################################################################################
 # Title:    get-eventLogs.ps1
 # Description: use this script to obatin event logs from the local computer
 # Author:   Matt Elliott
 # Date:     Oct 06, 2021
-# Modified: 
+# Modified: Nov 23, 2021 - added additional log collection and check for c:\temp folder
 ############################################################################################################
 
 # Self-elevate the script if required
@@ -15,16 +15,26 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
  }
 }
 try {
+    #check if c:\temp exists - create if not
+    if(!(Test-Path C:\temp)){ mkdir c:\temp}
     #Get-EventLog -LogName Application -ComputerName localhost 
-    $logsToGet = @("Application","Security","System")
+    $logsToGet = @("Application","Security","System","Windows PowerShell") 
     $computer = $ENV:COMPUTERNAME
     $path = "c:\temp"
     cd $path
-    #cd \\SDLAPDCDC1\WinEventLogs\
+    
     mkdir $computer
     Write-Host "Exporting logs from $computer"
     Foreach ($log in $logsToGet) {
     wevtutil epl $log "$path\$($computer)\$($computer)_($log).evtx" /remote:$computer /overwrite:true
+}
+# run through again with any logs with a / in the name since we also want to collect "Microsoft-Windows-PowerShell/Operational","Microsoft-Windows-Sysmon/Operational")
+    $logsToGet2 = @("Microsoft-Windows-PowerShell/Operational","Microsoft-Windows-Sysmon/Operational","Microsoft-Windows-WinRM/Operational") 
+    Write-Host "Exporting MORE logs from $computer"
+    Foreach ($log in $logsToGet2) {
+    [string]$logname = $log
+    $logname = $logname.Replace("/","-")
+    wevtutil epl $log "$path\$($computer)\$($computer)_($logname).evtx" /remote:$computer /overwrite:true
 }
     write-host "file saved to $path\$($computer)\ " -BackgroundColor Green -ForegroundColor Black
     write-host "Make sure to backup to secure location and delete from machine asap" -BackgroundColor Red -ForegroundColor yellow
